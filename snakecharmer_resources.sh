80573234ed32a92e96c91be0b5fb1870601be257
Kristen Wells <kwellswrasman@xsede.org@c3cpu-c11-u1-2.rc.int.colorado.edu>
2023-05-10 17:26:45 -0600

    adding option for resources addresses #2

--- snakecharmer_resources.sh
+++ snakecharmer_resources.sh
@@ -7,13 +7,13 @@
 #SBATCH --output=logs/snakemake_%J.out
 #SBATCH --partition=amilan
 
-profile="slurm_profiles/slurm_profile" # Or set to slurm_profile_singularity if using singularity
+profile="slurm_profiles/slurm_profile_resources" # Or set to slurm_profile_singularity if using singularity
 
 mkdir -p logs
 
 # Run snakemake pipeline
 snakemake \
-    --snakefile Snakefile \
+    --snakefile Snakefile_resources \
     --jobs 15 \
     --latency-wait 60 \
     --ignore-incomplete \
